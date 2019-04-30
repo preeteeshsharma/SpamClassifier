ed630af56f35258c65cacca0f033810e66b842f3
Preeteesh Sharma <prtshshrm@gmail.com>
2019-05-01 02:25:10 +0530

    Finish Feature Extraction Process. Split dataset into testing and training

--- App.py
+++ App.py
@@ -1,1 +1,19 @@
 #Main App
+from sklearn import model_selection
+from PreprocessText import *
+from FeatureExtract import *
+
+df=loadData('SMSSpamCollection')
+binary_labels=encodeMessages(df)
+preprocess_messages=preprocessMessages(df)
+#create bag of words from preporcess messages by tokenizing
+bag_of_words=createBag(preprocess_messages)
+#take the 1500 most common words as feature words
+feature_words=list(bag_of_words.keys())[:1500]
+#pass these feature words to a function that will find feature words in a ms=essage and return it along with its label
+zip_messages=zip(preprocess_messages, binary_labels)
+feature_sets=featureSetGenerator(feature_words, zip_messages)
+
+training, testing = model_selection.train_test_split(feature_sets, test_size = 0.25, random_state=1)
+print(len(training))
+print(len(testing))
--- FeatureExtract.py
+++ FeatureExtract.py
@@ -1,1 +1,30 @@
 #generate features
+#the words in the sms will be our features.
+import nltk
+from nltk.tokenize import word_tokenize
+import numpy as np
+
+#create a list which will contain all words from each message in process words
+def createBag(preprocess_messages):
+	bag_of_words=[]
+	for message in preprocess_messages:
+		words=word_tokenize(message)
+		for word in words:
+			bag_of_words.append(word)
+	bag_of_words=nltk.FreqDist(bag_of_words)
+	return bag_of_words
+
+#finds feature word in each message of dataset
+def featureFinder(message, feature_words):
+	#essentially a list with indexes as feature words. Index is true if feature word is present in msg else not
+	features_in_a_msg={}
+	w_tokens=word_tokenize(message)
+	for fw in feature_words:
+		features_in_a_msg[fw]=(fw in w_tokens)
+	return features_in_a_msg
+
+def featureSetGenerator(feature_words, zip_messages):
+	#np.random.seed = 1
+	#np.random.shuffle(zip_messages)
+	feature_sets = [(featureFinder(text, feature_words), label) for (text, label) in zip_messages]
+	return feature_sets
